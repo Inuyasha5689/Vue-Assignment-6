43ca8812ce83778d70cf0d345a1ab7a9c3092515
Drake <dascott0204@gmail.com>
2017-02-19 15:27:48 -0600

    Finished the assignment first try! was rather simple thing to do.

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <my-home></my-home>
+</template>
+
+<script>
+    import Home from './Home.vue';
+    export default {
+        components: {
+            'my-home': Home
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <my-header></my-header>
+        <hr>
+        <my-content></my-content>
+        <hr>
+        <my-footer></my-footer>
+    </div>
+</template>
+
+<script>
+    import Content from './Content.vue';
+    import Header from './Header.vue';
+    import Footer from './Footer.vue';
+    export default {
+        components: {
+            'my-header': Header,
+            'my-content': Content,
+            'my-footer': Footer
+        }
+    }
+</script>
